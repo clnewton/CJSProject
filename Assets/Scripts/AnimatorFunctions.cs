93844544b26dd113f96bedc8d7efc48b5cec2820
Clair Newton <clairlnewton@gmail.com>
2021-07-21 18:08:53 +0100

    Initial Commit

---
+++ Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+	[SerializeField] ButtonController buttonController;
+	public bool disableOnce;
+
+	void PlaySound(AudioClip whichSound){
+		if(!disableOnce){
+			buttonController.audioSource.PlayOneShot (whichSound);
+		}else{
+			disableOnce = false;
+		}
+	}
+}	
---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+	[SerializeField] ButtonController buttonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+
+    public string scene;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(buttonController.index == thisIndex)
+		{
+			animator.SetBool ("selected", true);
+			if(Input.GetAxis ("Submit") == 1){
+				animator.SetBool ("pressed", true);
+			}else if (animator.GetBool ("pressed")){
+				animator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+                SceneManager.LoadScene(scene);
+			}
+		}else{
+			animator.SetBool ("selected", false);
+		}
+    }
+}
